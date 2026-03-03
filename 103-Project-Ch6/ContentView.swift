eeeb5b2f4e20dd8253cbf46ec38e1488e5e5965a
Jeffrey Lullen <lullenj@gmail.com>
2026-03-03 09:23:38 +0800

    Initial Commit

---
+++ 103-Project-Ch6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  103-Project-Ch6
+//
+//  Created by jeffrey lullen on 3/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 103-Project-Ch6/_03_Project_Ch6App.swift
@@ -0,0 +1,17 @@
+//
+//  _03_Project_Ch6App.swift
+//  103-Project-Ch6
+//
+//  Created by jeffrey lullen on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct _03_Project_Ch6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
